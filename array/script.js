80120fe033d6d36884a0d24ddf04a184e8691e7a
luis4PF <luishtardem@gmail.com>
2022-09-05 21:59:05 -0300

    commit

---
+++ array/script.js
@@ -0,0 +1,18 @@
+let btnAdicionar = document.getElementById("btnAdicionar");
+btnAdicionar.addEventListener("click", function () {
+
+    let elementos = [];
+    let valor = document.getElementById("valor").value;
+    elementos.push(valor.toString());
+
+    let elementoOL = document.createElement("OL");
+    elementos.forEach(element => {
+        let elementLI = document.createElement("LI");
+        let textNode = document.createTextNode(element);
+        elementLI.appendChild(textNode);
+
+        elementoOL.appendChild(textNode);
+    });
+
+    document.body.appendChild(elementoOL);
+});
